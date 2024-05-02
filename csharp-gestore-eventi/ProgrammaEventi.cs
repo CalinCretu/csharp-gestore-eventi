9da9ba6520da28a210882d0c6acdcc5fce527ef0
CalinCretu <calincretu1412@gmail.com>
2024-05-02 14:25:14 +0200

    ADD - metodo AggiungiEvento()

---
+++ csharp-gestore-eventi/ProgrammaEventi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    internal class ProgrammaEventi
+    {
+        public string Titolo { get; private set; }
+        private List<Evento> eventi;
+
+        public ProgrammaEventi(string titolo)       // costruttore
+        {
+            Titolo = titolo;
+            eventi = new List<Evento>();
+        }
+
+        public void AggiungiEvento(Evento nuovoEvento)      // metodo per aggiungere un nuovo evento
+        {
+            eventi.Add(nuovoEvento);
+        }
+    }
+}
